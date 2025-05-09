8f0fe4b3f6d0bde44d222cb417a7dfc2613514e0
Tandat3177 <datt35774@gmail.com>
2025-05-09 19:56:53 +0700

    Add files via upload

---
+++ Datdepzai.lua
@@ -0,0 +1,16 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Tandat3177/Dead-Rails-v-Redz-hub/refs/heads/main/Scriptdemo.lua.txt"))()
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Thông Báo")
+root.geometry("300x100")
+
+label = tk.Label(root, text="Cali con cặc tuổi lồn", font=("Arial", 20), fg="yellow")
+label.pack(pady=20)
+
+def close_window():
+    root.after(3000, root.destroy)
+
+close_window()
+
+root.mainloop()
